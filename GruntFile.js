6af85ac7e93b925f79854c5b65b2611b3cd3c67f
Scott Davis <stdavis@utah.gov>
2016-11-25 13:14:44 -0700

    config files + grunt

---
+++ GruntFile.js
@@ -0,0 +1,335 @@
+module.exports = function configure(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    var jsAppFiles = 'src/app/**/*.js';
+    var otherFiles = [
+        'src/app/**/*.html',
+        'src/app/**/*.css',
+        'src/index.html',
+        'src/ChangeLog.html'
+    ];
+    var gruntFile = 'GruntFile.js';
+    var jsFiles = [
+        jsAppFiles,
+        gruntFile,
+        'profiles/**/*.js'
+    ];
+    var bumpFiles = [
+        'package.json',
+        'bower.json',
+        'src/app/package.json',
+        'src/app/config.js'
+    ];
+    var deployFiles = [
+        '**',
+        '!**/*.uncompressed.js',
+        '!**/*consoleStripped.js',
+        '!**/bootstrap/less/**',
+        '!**/bootstrap/test-infra/**',
+        '!**/tests/**',
+        '!build-report.txt',
+        '!components-jasmine/**',
+        '!favico.js/**',
+        '!jasmine-favicon-reporter/**',
+        '!jasmine-jsreporter/**',
+        '!stubmodule/**',
+        '!util/**'
+    ];
+    var deployDir = 'roads';
+    var secrets;
+
+    try {
+        secrets = grunt.file.readJSON('secrets.json');
+    } catch (e) {
+        // swallow for build server
+
+        // still print a message so you can catch bad syntax in the secrets file.
+        grunt.log.write(e);
+
+        secrets = {
+            stage: {
+                host: '',
+                username: '',
+                password: ''
+            },
+            prod: {
+                host: '',
+                username: '',
+                password: ''
+            }
+        };
+    }
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bump: {
+            options: {
+                files: bumpFiles,
+                commitFiles: bumpFiles.concat('src/ChangeLog.html'),
+                push: false
+            }
+        },
+        clean: {
+            build: ['dist'],
+            deploy: ['deploy']
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'deploy/deploy.zip'
+                },
+                files: [{
+                    src: deployFiles,
+                    dest: './',
+                    cwd: 'dist/',
+                    expand: true
+                }]
+            }
+        },
+        connect: {
+            uses_defaults: { // eslint-disable-line camelcase
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['*.html'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        dojo: {
+            prod: {
+                options: {
+                    // You can also specify options to be used in all your tasks
+                    profiles: ['profiles/prod.build.profile.js', 'profiles/build.profile.js']
+                }
+            },
+            stage: {
+                options: {
+                    // You can also specify options to be used in all your tasks
+                    profiles: ['profiles/stage.build.profile.js', 'profiles/build.profile.js']
+                }
+            },
+            options: {
+                // You can also specify options to be used in all your tasks
+                // Path to dojo.js file in dojo source
+                dojo: 'src/dojo/dojo.js',
+                // Optional: Utility to bootstrap (Default: 'build')
+                load: 'build',
+                releaseDir: '../dist',
+                // Optional: Module to require for the build (Default: nothing)
+                requires: ['src/app/packages.js', 'src/app/run.js'],
+                basePath: './src'
+            }
+        },
+        eslint: {
+            options: {
+                configFile: '.eslintrc'
+            },
+            main: {
+                src: jsFiles
+            }
+        },
+        imagemin: {
+            main: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    // exclude tests because some images in dojox throw errors
+                    src: ['**/*.{png,jpg,gif}', '!**/tests/**/*.*'],
+                    dest: 'src/'
+                }]
+            }
+        },
+        jasmine: {
+            main: {
+                options: {
+                    specs: ['src/app/**/Spec*.js'],
+                    vendor: [
+                        'src/jasmine-favicon-reporter/vendor/favico.js',
+                        'src/jasmine-favicon-reporter/jasmine-favicon-reporter.js',
+                        'src/jasmine-jsreporter/jasmine-jsreporter.js',
+                        'src/app/tests/jasmineTestBootstrap.js',
+                        'src/dojo/dojo.js',
+                        'src/app/packages.js',
+                        'src/app/tests/jsReporterSanitizer.js',
+                        'src/app/tests/jasmineAMDErrorChecking.js'
+                    ],
+                    host: 'http://localhost:8000'
+                }
+            }
+        },
+        parallel: {
+            options: {
+                grunt: true
+            },
+            assets: {
+                tasks: ['eslint:main', 'stylus', 'jasmine:main:build']
+            },
+            buildAssets: {
+                tasks: ['eslint:main', 'clean:build', 'newer:imagemin:main', 'stylus']
+            }
+        },
+        processhtml: {
+            options: {},
+            main: {
+                files: {
+                    'dist/index.html': ['src/index.html'],
+                    'dist/user_admin.html': ['src/user_admin.html']
+                }
+            }
+        },
+        secrets: secrets,
+        sftp: {
+            stage: {
+                files: {
+                    './': 'deploy/deploy.zip'
+                },
+                options: {
+                    host: '<%= secrets.stage.host %>',
+                    username: '<%= secrets.stage.username %>',
+                    password: '<%= secrets.stage.password %>'
+                }
+            },
+            prod: {
+                files: {
+                    './': 'deploy/deploy.zip'
+                },
+                options: {
+                    host: '<%= secrets.prod.host %>',
+                    username: '<%= secrets.prod.username %>',
+                    password: '<%= secrets.prod.password %>',
+                    path: './upload/' + deployDir
+                }
+            },
+            options: {
+                createDirectories: true,
+                path: './wwwroot/' + deployDir + '/',
+                srcBasePath: 'deploy/',
+                showProgress: true
+            }
+        },
+        sshexec: {
+            options: {
+
+            },
+            stage: {
+                command: ['cd wwwroot/' + deployDir, 'unzip -oq deploy.zip', 'rm deploy.zip'].join(';'),
+                options: {
+                    host: '<%= secrets.stage.host %>',
+                    username: '<%= secrets.stage.username %>',
+                    password: '<%= secrets.stage.password %>'
+                }
+            },
+            prod: {
+                command: ['cd wwwroot/' + deployDir, 'unzip -oq deploy.zip', 'rm deploy.zip'].join(';'),
+                options: {
+                    host: '<%= secrets.prod.host %>',
+                    username: '<%= secrets.prod.username %>',
+                    password: '<%= secrets.prod.password %>'
+                }
+            }
+        },
+        stylus: {
+            main: {
+                options: {
+                    compress: false,
+                    'resolve url': true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['app/**/*.styl'],
+                    dest: 'src/',
+                    ext: '.css'
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                preserveComments: false,
+                sourceMap: true,
+                compress: {
+                    drop_console: true, // eslint-disable-line camelcase
+                    passes: 2,
+                    dead_code: true // eslint-disable-line camelcase
+                }
+            },
+            stage: {
+                options: {
+                    compress: {
+                        drop_console: false // eslint-disable-line camelcase
+                    }
+                },
+                src: ['dist/dojo/dojo.js'],
+                dest: 'dist/dojo/dojo.js'
+            },
+            prod: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist',
+                    src: '**/*.js',
+                    dest: 'dist'
+                }]
+            }
+        },
+        watch: {
+            eslint: {
+                files: jsFiles,
+                tasks: ['eslint:main', 'jasmine:main:build']
+            },
+            src: {
+                files: jsFiles.concat(otherFiles),
+                options: { livereload: true }
+            },
+            stylus: {
+                files: 'src/app/**/*.styl',
+                tasks: ['stylus']
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'parallel:assets',
+        'connect',
+        'watch'
+    ]);
+    grunt.registerTask('build-prod', [
+        'parallel:buildAssets',
+        'dojo:prod',
+        'uglify:prod',
+        'copy:main',
+        'processhtml:main'
+    ]);
+    grunt.registerTask('deploy-prod', [
+        'clean:deploy',
+        'compress:main',
+        'sftp:prod'
+    ]);
+    grunt.registerTask('build-stage', [
+        'parallel:buildAssets',
+        'dojo:stage',
+        'uglify:stage',
+        'copy:main',
+        'processhtml:main'
+    ]);
+    grunt.registerTask('deploy-stage', [
+        'clean:deploy',
+        'compress:main',
+        'sftp:stage',
+        'sshexec:stage'
+    ]);
+    grunt.registerTask('travis', [
+        'jasmine',
+        'eslint:main',
+        'sauce',
+        'build-stage'
+    ]);
+};
