43e0e5c7796f7c066503c4f40f90beec4a5cf832
Scott Davis <stdavis@utah.gov>
2016-11-25 13:17:54 -0700

    build profiles

---
+++ profiles/build.profile.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+var profile = {
+    basePath: '../src',
+    action: 'release',
+    cssOptimize: 'comments',
+    mini: true,
+    optimize: false,
+    layerOptimize: false,
+    selectorEngine: 'acme',
+    layers: {
+        'dojo/dojo': {
+            include: [
+                'dojo/i18n',
+                'dojo/domReady',
+                'app/packages',
+                'app/run',
+                'app/App',
+                'dojox/gfx/filters',
+                'dojox/gfx/path',
+                'dojox/gfx/svg',
+                'dojox/gfx/svgext',
+                'dojox/gfx/shape'
+            ],
+            targetStylesheet: 'app/resources/App.css',
+            includeLocales: ['en-us'],
+            customBase: true,
+            boot: true
+        },
+        'ijit/widgets/authentication/UserAdmin': {
+            exclude: ['dojo/dojo']
+        }
+    },
+    packages: [{
+        name: 'moment',
+        location: 'moment',
+        main: 'moment',
+        trees: [
+          // don't bother with .hidden, tests, min, src, and templates
+          ['.', '.', /(\/\.)|(~$)|(test|txt|src|min|templates)/]
+        ],
+        resourceTags: {
+            amd: function amd(filename, mid) {
+                return /\.js$/.test(filename);
+            }
+        }
+    }],
+    staticHasFeatures: {
+        // The trace & log APIs are used for debugging the loader, so we don’t need them in the build
+        'dojo-trace-api': 0,
+        'dojo-log-api': 0,
+
+        // This causes normally private loader data to be exposed for debugging, so we don’t need that either
+        'dojo-publish-privates': 0,
+
+        // We’re fully async, so get rid of the legacy loader
+        'dojo-sync-loader': 0,
+
+        // dojo-xhr-factory relies on dojo-sync-loader
+        'dojo-xhr-factory': 0,
+
+        // We aren’t loading tests in production
+        'dojo-test-sniff': 0
+    },
+    userConfig: {
+        packages: ['app', 'dijit', 'dojox', 'agrc', 'ijit', 'esri', 'layer-selector']
+    }
+};
