d3f0d2984e46133425d55d57a3d52aa54218479e
Scott Davis <stdavis@utah.gov>
2017-01-20 12:59:35 -0700

    Release v2.0.0-10

--- src/app/config.js
+++ src/app/config.js
@@ -25,7 +25,7 @@ define([
     window.AGRC = {
         // version.: String
         //      The version number.
-        version: '2.0.0-9',
+        version: '2.0.0-10',
         appName: 'plpco',
 
         // app: App
