caa7090a3c9c4b1e8db678f78040549803df7e9b
Scott Davis <stdavis@utah.gov>
2017-07-03 08:58:54 -0600

    remove src data - fixup into babelify

--- src/app/run.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    require({ baseUrl: './' }, ['dojo/parser', 'jquery', 'dojo/domReady!'], function (parser) {
-        parser.parse();
-    });
-})();
-//# sourceMappingURL=run.js.map
